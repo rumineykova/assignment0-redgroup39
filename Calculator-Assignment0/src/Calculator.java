47ee83a681217bfc5a09fb5c7ec1da11514b7236
anapoleac <75444152+anapoleac@users.noreply.github.com>
2020-12-09 16:50:03 +0000

    Update Calculator.java

--- Calculator-Assignment0/src/Calculator.java
+++ Calculator-Assignment0/src/Calculator.java
@@ -79,6 +79,41 @@ public class Calculator {
 
 	        ScannerInput.closeScanner();
 	    }
+	
+	public static void subtask5() {
+		int decimal;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter a positive integer: ");
+		decimal = in.nextInt();
+		int input;
+		int binary[] = new int[50];
+		int index = 0;
+		input = decimal;
+		
+		if(decimal < 0 )
+		{
+			System.out.println("Error: number not positive, try again");
+		}
+		else
+		{
+			if(decimal <= 1)
+			{
+				System.out.println("0 is the binary form for " + input);
+			}
+			else
+			{
+				while(decimal > 0)
+				{
+					binary[index] = decimal%2;
+					decimal = decimal/2;
+					index++;
+				}
+				for(int i = index - 1; i>=0;i--)
+					System.out.print(binary[i]);
+					System.out.print(" is the binary form for " + input);
+			}
+		}
+	}
 	 
 	public static void subtask6() {
 		System.out.println("Enter a binary number");
