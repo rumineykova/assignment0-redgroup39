7e01534985d7fc960672fe3859fee5f6eba52c24
rumi <rumi.neykova@gmail.com>
2020-12-10 16:42:03 +0000

    Move Task5 task to a separate class

--- Calculator-Assignment0/src/Calculator.java
+++ Calculator-Assignment0/src/Calculator.java
@@ -25,6 +25,9 @@ public class Calculator {
 		case 4:
 			subtask4();
 			break;
+		case 5:
+			subtask5();
+			break;
 		case 6: 
 			subtask6(); 
 			break;
@@ -91,38 +94,7 @@ public class Calculator {
 	    }
 	
 	public static void subtask5() {
-		int decimal;
-		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter a positive integer: ");
-		decimal = in.nextInt();
-		int input;
-		int binary[] = new int[50];
-		int index = 0;
-		input = decimal;
-		
-		if(decimal < 0 )
-		{
-			System.out.println("Error: number not positive, try again");
-		}
-		else
-		{
-			if(decimal <= 1)
-			{
-				System.out.println("0 is the binary form for " + input);
-			}
-			else
-			{
-				while(decimal > 0)
-				{
-					binary[index] = decimal%2;
-					decimal = decimal/2;
-					index++;
-				}
-				for(int i = index - 1; i>=0;i--)
-					System.out.print(binary[i]);
-					System.out.print(" is the binary form for " + input);
-			}
-		}
+		Task5.decimalToBinary();
 	}
 	 
 	public static void subtask6() {
